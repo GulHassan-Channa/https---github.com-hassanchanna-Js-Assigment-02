d07fab0962cc2133f79f5c17bb4a24e9652de637
hassanchanna <tanhapanchi484@gmail.com>
2022-11-03 16:32:12 +0500

    done

---
+++ app.js
@@ -0,0 +1,56 @@
+//  ../   Prompt ../
+
+
+// var nameFirst = prompt ("Enter Your First Name");
+// var nameLast = prompt ("Enter Your First Name")
+// var fullName = (nameFirst+nameLast);
+// console.log(fullName);
+
+// var name =prompt ("Enter Your Name");
+// alert(name)
+// var name = "Hassan";
+// if(name == "Hassan"){
+//     console.log("Well come to Karachi")
+// } 
+
+
+// var num = 12;
+// if (num ==12){
+//     alert("Your number is correct")
+// }
+//...//
+
+
+
+// var name = "Hassan";
+// if(name != "Hassan"){
+//     console.log("Well come to Karachi")
+// }else {
+//     console.log("User Not match")
+// }
+
+
+// var  num = 1001;
+// if(num >= 999){
+//     alert("Value is less then 1001")
+// } else{
+//     alert("Value is Greater then num")
+// }
+
+
+
+
+// var name = prompt("Where are u from")
+// if (name === "Karachi"){
+//     console.log("Well come pa ji")
+// } else {
+//     console.log("U Not Well come ")
+// }
+
+// var where = prompt("Which City is best in Sindh?");
+//  if (where === "Karachi") {
+//  alert("Correct Good");
+//  }
+//  if (where !== "Karachi") {
+//  alert("Sahi Value Dal bahi");
+//  }
